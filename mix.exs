f0f0808a28efa60b397b481669dfa27f75090e6e
19年梦醒 <3949379+getong@users.noreply.github.com>
2019-12-19 00:04:45 +0800

    add geo package

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule PhoenixExample.MixProject do
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:myxql, ">= 0.0.0"},
+      {:geo, "~> 3.3"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
