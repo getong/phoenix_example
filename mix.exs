d32ab0305c7c950eebffd19c1cf4784eea8a7e4d
19年梦醒 <3949379+getong@users.noreply.github.com>
2019-12-21 16:04:03 +0800

    use elastix

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule PhoenixExample.MixProject do
       {:quantum, "~> 2.3"},
       {:logger_file_backend, "~> 0.0.11"},
       {:distillery, "~> 2.1.0", runtime: false},
-      {:elastic, "~> 3.5"},
+      {:elastix, "~> 0.7.1"},
       {:libcluster, "~> 3.1"},
       {:redix, "~> 0.10.4"}
     ]
