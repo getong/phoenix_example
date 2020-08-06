400f39098043202b73094967b6827611aaf4c647
19年梦醒 <3949379+getong@users.noreply.github.com>
2020-08-06 23:52:01 +0800

    add bolt_sips library

--- mix.exs
+++ mix.exs
@@ -72,7 +72,8 @@ defmodule PhoenixExample.MixProject do
       {:cowlib, "~> 2.9", override: true},
       {:gun, "~> 1.3", override: true},
       {:emqtt, "~> 1.2"},
-      {:oban, "~> 2.0"}
+      {:oban, "~> 2.0"},
+      {:bolt_sips, "~> 2.0"}
     ]
   end
 
